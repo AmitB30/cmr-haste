ff040e7ff933cdf4f4c8c6918142a091c2c38a7c
AmitB30 <93730389+AmitB30@users.noreply.github.com>
2022-03-14 21:13:21 +0000

    Update checkpoint_saver.py

--- cmr_haste/utils/checkpoint_saver.py
+++ cmr_haste/utils/checkpoint_saver.py
@@ -72,7 +72,8 @@ class CheckpointSaver:
                 self._cleanup_checkpoints(1)
             filename = '-'.join([self.save_prefix, str(epoch)]) + self.extension
             save_path = os.path.join(self.checkpoint_dir, filename)
-            os.link(last_save_path, save_path)
+            #os.link(last_save_path, save_path)
+            last_save_path = save_path
             self.checkpoint_files.append((save_path, metric))
             self.checkpoint_files = sorted(
                 self.checkpoint_files, key=lambda x: x[1],
